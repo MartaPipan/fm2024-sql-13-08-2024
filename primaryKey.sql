e33fc91a98638a3963bcac36fb3b524462a74188
Marta Prystatska <tradlisboa@gmail.com>
2024-08-18 21:58:29 +0100

    Add bigserial, "id" PRIMARY KEY

---
+++ primaryKey.sql
@@ -0,0 +1,16 @@
+--massages
+--body, author, createAt, isRead
+DROP TABLE IF EXISTS "messages";
+
+CREATE TABLE IF NOT EXISTS "messages"(
+    "id" bigserial PRIMARY KEY,
+    "body" varchar(2048) NOT NULL CHECK("body"!=''),
+    "author" varchar(32) NOT NULL CHECK("author"!=''),
+    "created_at" timestamp NOT NULL DEFAULT current_timestamp,
+    "is_read" boolean DEFAULT false
+);
+INSERT INTO "messages"("body", "author")
+VALUES
+('hi','Tom'),
+('hi','Tom'),
+('hello', 'Alex');
