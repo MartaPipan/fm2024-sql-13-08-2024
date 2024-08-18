7dc5bb7b71671ef0d9078c6afb5eef163b14c19c
Marta Prystatska <tradlisboa@gmail.com>
2024-08-18 22:18:28 +0100

    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";    "id" uuid PRIMARY KEY DEFAULT uuid_generate_v1(),

--- primaryKey.sql
+++ primaryKey.sql
@@ -1,15 +1,18 @@
---massages
---body, author, createAt, isRead
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
 DROP TABLE IF EXISTS "messages";
 
 CREATE TABLE IF NOT EXISTS "messages"(
-    "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    "id" uuid PRIMARY KEY DEFAULT uuid_generate_v1(),
     "body" varchar(2048) NOT NULL CHECK("body"!=''),
     "author" varchar(32) NOT NULL CHECK("author"!=''),
     "created_at" timestamp NOT NULL DEFAULT current_timestamp,
     "is_read" boolean DEFAULT false
 );
-INSERT INTO "messages"("body", "author")
+
+INSERT INTO "messages"("body", "author") 
 VALUES
 ('hi','Tom'),
 ('hi','Tom'),
