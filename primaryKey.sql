e4c16773e50adb25f3e89490631c76dfe3703f3e
Marta Prystatska <tradlisboa@gmail.com>
2024-08-18 22:07:31 +0100

    Add uuid :  "id" uuid PRIMARY KEY DEFAULT gen_random_uuid()

--- primaryKey.sql
+++ primaryKey.sql
@@ -3,7 +3,7 @@
 DROP TABLE IF EXISTS "messages";
 
 CREATE TABLE IF NOT EXISTS "messages"(
-    "id" bigserial PRIMARY KEY,
+    "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     "body" varchar(2048) NOT NULL CHECK("body"!=''),
     "author" varchar(32) NOT NULL CHECK("author"!=''),
     "created_at" timestamp NOT NULL DEFAULT current_timestamp,
