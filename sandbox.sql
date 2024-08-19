b07583ada324018b5cca3c06e3fc563b299b1acd
Marta Prystatska <tradlisboa@gmail.com>
2024-08-19 07:56:09 +0100

    add CONSTRAINT "uniquePair" PRIMARY KEY(b,c)

---
+++ sandbox.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS a;
+
+CREATE TABLE IF NOT EXISTS a(
+    b int CHECK(b<10) NOT NULL,
+    c int NOT NULL,
+    CONSTRAINT "uniquePair" PRIMARY KEY(b,c)
+    );
+
+INSERT INTO a VALUES (1,1),(2,1),(3,1);
