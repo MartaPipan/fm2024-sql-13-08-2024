09f4ff1181de748aef5f12cd6ea39ceb11475b96
Marta Prystatska <tradlisboa@gmail.com>
2024-08-19 08:59:26 +0100

    add PRIMARY KEY

--- primaryKey.sql
+++ primaryKey.sql
@@ -4,13 +4,13 @@ DROP TABLE IF EXISTS "messages";
 CREATE TABLE IF NOT EXISTS "messages"(
     "id" bigserial PRIMARY KEY,
     "body" varchar(2048) NOT NULL CHECK("body"!=''),
-    "author" varchar(32) NOT NULL CHECK("author"!=''),
+    "author_id" int REFERENCES "users"("id"),
     "created_at" timestamp NOT NULL DEFAULT current_timestamp,
     "is_read" boolean DEFAULT false
 );
 
-INSERT INTO "messages"("body", "author") 
+INSERT INTO "messages"("body", "author_id") 
 VALUES
-('hi','Tom'),
-('hi','Tom'),
-('hello', 'Alex');
+('hi', 3),
+('hi', 1),
+('hello', 2);
