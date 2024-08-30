7258464d31c715a74e2c8c2c72157bda1bab21e1
Marta Prystatska <tradlisboa@gmail.com>
2024-08-30 18:51:30 +0100

    add PG Client

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const { Client } = require('pg');
+const client = new Client({
+  user: 'postgres',
+  password: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  database: 'test24',
+});
 
 
-
+async function start(params) {
+    try {
+        await client.connect();
+        const response = await client.query('CREATE TABLE "from_js"("id"serial PRIMARY KEY);');
+        console.log(response);
+        await client.end();
+    } catch (error) {
+      console.log(error);
+    }
+}
+start()
